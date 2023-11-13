713cf16997c1fb91a128f195e02043b7f88d2dd7
AlexanderPAI <lawlikeart@yandex.ru>
2023-11-14 01:34:11 +0300

    Getting query_params form Request

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 
 app = FastAPI()
 
 
 @app.post('/get_form')
-def get_form() -> dict[str, str]:
+def get_form(request: Request) -> dict[str, str]:
     """Эндпоинт для определения шаблона формы."""
-    return {'test': 'it is working'}
+    params = request.query_params
+    return params
