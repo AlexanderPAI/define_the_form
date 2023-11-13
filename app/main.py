71fb03c0ef4e241718da94d6795417690fbbfd8b
AlexanderPAI <lawlikeart@yandex.ru>
2023-11-14 02:06:12 +0300

    Added validators for date, phone, email

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import re
+
 from fastapi import FastAPI, Request
 
 
@@ -6,6 +8,27 @@ class Form:
 
     def __init__(self, *args):
         self.__dict__.update(*args)
+
+    def is_date(self, date: str) -> bool:
+        """Валидатор поля 'Дата'."""
+        regex_pattern = r'^\d{2}\.\d{2}\.\d{4}'
+        if re.fullmatch(regex_pattern, date):
+            return True
+        return False
+
+    def is_phone(self, phone: str) -> bool:
+        """Валидатор поля 'Номер телефона'."""
+        regex_pattern = r'^\+7\s\d{3}\s\d{3}\s\d{2}\s\d{2}$'
+        if re.fullmatch(regex_pattern, phone):
+            return True
+        return False
+
+    def is_email(self, email: str) -> bool:
+        """Валидатор поля 'Email'."""
+        regex_pattern = r'(\w+[.-_])*\w+@\w+(\.[A-Z|a-z]{2,})+'
+        if re.match(regex_pattern, email):
+            return True
+        return False
 
     def __str__(self):
         return 'Форма'
