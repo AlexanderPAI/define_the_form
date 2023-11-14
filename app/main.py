1eff6aeec2c0f6954d2753563519931d6779c5ec
AlexanderPAI <lawlikeart@yandex.ru>
2023-11-14 03:32:25 +0300

    Refactoring: func get_form moved to app.services.endpoints; added router to main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,9 @@
 from fastapi import FastAPI, Request
 
-from app.services.db import db
-from app.services.classes import Form
+from app.services.endpoints import router
 
 
 app = FastAPI()
 
 
-@app.post('/get_form')
-def get_form(request: Request):
-    """Эндпоинт для определения шаблона формы."""
-    form = Form(request.query_params)
-    template_names = form.find_template_in_db(db)
-    if template_names:
-        return template_names
-    return form.get_form_template()
+app.include_router(router)
---
+++ app/services/endpoints.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Request
+
+from app.services.db import db
+from app.services.classes import Form
+
+
+router = APIRouter()
+
+
+@router.post('/get_form')
+def get_form(request: Request):
+    """Эндпоинт для определения шаблона формы."""
+    form = Form(request.query_params)
+    template_names = form.find_template_in_db(db)
+    if template_names:
+        return template_names
+    return form.get_form_template()
